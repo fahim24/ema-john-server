396e614f1de8f9960877e67f58f5d7dce0e5d7ea
aKai <fahim.ak24@gmail.com>
2022-04-30 17:07:56 +0600

    ema-john db server

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7totj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+	try {
+		await client.connect();
+		const productsCollection = client.db("emaJohn").collection("products");
+		console.log("db is connected");
+
+		app.get("/products", async (req, res) => {
+			console.log("query", req.query);
+			const page = parseInt(req.query.page);
+			const size = parseInt(req.query.size);
+
+			const query = {};
+			const cursor = productsCollection.find(query);
+			let products;
+			if (page || size) {
+				// 0 --> skip: 0 get: 0-10(10):
+				// 1 --> skip: 1*10 get: 11-20(10):
+				// 2 --> skip: 2*10 get: 21-30 (10):
+				// 3 --> skip: 3*10 get: 21-30 (10):
+				products = await cursor
+					.skip(page * size)
+					.limit(size)
+					.toArray();
+			} else {
+				products = await cursor.toArray();
+			}
+
+			res.send(products);
+		});
+
+		app.get("/productCount", async (req, res) => {
+			const count = await productsCollection.estimatedDocumentCount();
+			res.send({ count });
+		});
+
+		// use post to get products by ids
+		app.post("/productByKeys", async (req, res) => {
+			const keys = req.body;
+			const ids = keys.map((id) => ObjectId(id));
+			const query = { _id: { $in: ids } };
+			const cursor = productsCollection.find(query);
+			const products = await cursor.toArray();
+			console.log(keys);
+			res.send(products);
+		});
+	} finally {
+	}
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+	res.send("ema-jhon server is running");
+});
+
+app.listen(port, () => {
+	console.log("server listening to port:", port);
+});
